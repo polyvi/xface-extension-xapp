4c7647deb75c57b7d08da5aa0a9aad5b182b04ca
Yangtb <yangtb@polyvi.com>
2013-09-26 14:27:43 +0800

    add wp8 app extension

---
+++ www/wp8/app.js
@@ -0,0 +1,18 @@
+/**
+ * @module app
+ */
+var argscheck = require('cordova/argscheck'),
+    exec = require('cordova/exec');
+var app = {};
+
+
+app.exitApp = function(){
+    //closeApplication
+    exec(null, null, "App", "exitApp", []);
+};
+
+app.backHistory = function(successCallback, errorCallback){
+    argscheck.checkArgs('FF', 'App.backHistory', arguments);
+    exec(successCallback, errorCallback, "App", "backHistory", []);
+};
+module.exports = app;
