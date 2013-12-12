c1be8a039c22dd14fdc66e0fd1a0defcd2378aa1
luoqpolyvi <luoq@polyvi.com>
2013-12-12 15:16:58 +0800

    added jasmine tests unique id

--- test/autotest/tests/app.tests.js
+++ test/autotest/tests/app.tests.js
@@ -1,24 +1,24 @@
 describe('App (navigator.app)', function () {
-    it("should exist", function () {
+    it("app.spec.1 should exist app", function () {
         expect(navigator.app).toBeDefined();
     });
 
     describe("openUrl", function() {
-        it("should exist", function() {
+        it("app.spec.2 should exist openUrl", function() {
             expect(typeof navigator.app.openUrl).toBeDefined();
             expect(typeof navigator.app.openUrl == 'function').toBe(true);
         });
     });
     if (isAndroid() || isWindowsPhone()) {
         describe("exitApp", function() {
-                it("should exist", function() {
+                it("app.spec.3 should exist exitApp", function() {
                     expect(typeof navigator.app.exitApp).toBeDefined();
                     expect(typeof navigator.app.exitApp == 'function').toBe(true);
                 });
         });
 
         describe("backHistory", function() {
-                it("should exist", function() {
+                it("app.spec.4 should exist backHistory", function() {
                     expect(typeof navigator.app.backHistory).toBeDefined();
                     expect(typeof navigator.app.backHistory == 'function').toBe(true);
                 });
@@ -26,21 +26,21 @@ describe('App (navigator.app)', function () {
     }
     if (isAndroid()) {
         describe("install", function() {
-                it("should exist", function() {
+                it("app.spec.5 should exist install", function() {
                     expect(typeof navigator.app.install).toBeDefined();
                     expect(typeof navigator.app.install == 'function').toBe(true);
                 });
         });
 
         describe("clearHistory", function() {
-                it("should exist", function() {
+                it("app.spec.6 should exist clearHistory", function() {
                     expect(typeof navigator.app.clearHistory).toBeDefined();
                     expect(typeof navigator.app.clearHistory == 'function').toBe(true);
                 });
         });
 
         describe("clearCache", function() {
-                it("should exist", function() {
+                it("app.spec.7 should exist clearCache", function() {
                     expect(typeof navigator.app.clearCache).toBeDefined();
                     expect(typeof navigator.app.clearCache == 'function').toBe(true);
                 });
